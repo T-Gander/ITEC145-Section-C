1d8376182c00f0c1d898b6e0e5e43ff483f6d891
Thomas Gander - School <thomas.gander@mymhc.ca>
2023-01-23 12:00:01 -0700

    Finished adding the datagrid, loading a file + splitting.

--- ITEC145--Section-C--/Form1.Designer.cs
+++ ITEC145--Section-C--/Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.txtRead = new System.Windows.Forms.TextBox();
             this.txtCopy = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AddressLine1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.City = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Province = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Postcode = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,6 +60,7 @@
             this.btnToGrid.TabIndex = 1;
             this.btnToGrid.Text = "Move data from List to Grid";
             this.btnToGrid.UseVisualStyleBackColor = true;
+            this.btnToGrid.Click += new System.EventHandler(this.btnToGrid_Click);
             // 
             // txtRead
             // 
@@ -72,11 +79,49 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FirstName,
+            this.Surname,
+            this.AddressLine1,
+            this.City,
+            this.Province,
+            this.Postcode});
             this.dataGridView1.Location = new System.Drawing.Point(12, 130);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
             this.dataGridView1.Size = new System.Drawing.Size(539, 192);
             this.dataGridView1.TabIndex = 4;
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "First Name";
+            this.FirstName.Name = "FirstName";
+            // 
+            // Surname
+            // 
+            this.Surname.HeaderText = "Surname";
+            this.Surname.Name = "Surname";
+            // 
+            // AddressLine1
+            // 
+            this.AddressLine1.HeaderText = "Address Line 1";
+            this.AddressLine1.Name = "AddressLine1";
+            this.AddressLine1.Width = 110;
+            // 
+            // City
+            // 
+            this.City.HeaderText = "City";
+            this.City.Name = "City";
+            // 
+            // Province
+            // 
+            this.Province.HeaderText = "Province";
+            this.Province.Name = "Province";
+            // 
+            // Postcode
+            // 
+            this.Postcode.HeaderText = "Postcode";
+            this.Postcode.Name = "Postcode";
             // 
             // Form1
             // 
@@ -89,7 +134,7 @@
             this.Controls.Add(this.btnToGrid);
             this.Controls.Add(this.btnLoad);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Loading Lists Form";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -103,5 +148,11 @@
         private TextBox txtRead;
         private TextBox txtCopy;
         private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn FirstName;
+        private DataGridViewTextBoxColumn Surname;
+        private DataGridViewTextBoxColumn AddressLine1;
+        private DataGridViewTextBoxColumn City;
+        private DataGridViewTextBoxColumn Province;
+        private DataGridViewTextBoxColumn Postcode;
     }
 }
--- ITEC145--Section-C--/Form1.cs
+++ ITEC145--Section-C--/Form1.cs
@@ -1,7 +1,14 @@
 namespace ITEC145__Section_C__
 {
+    
+
+
     public partial class Form1 : Form
     {
+        List<string> strings = new List<string>();
+        OpenFileDialog ofd = new OpenFileDialog();
+        const int STRINGSPLITCOUNT = 6;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,11 +16,95 @@ namespace ITEC145__Section_C__
 
         private void btnLoad_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
+            try
+            {
+                int count = 0;
+                ofd.ShowDialog();     
+                
+                //txtRead.Text = ofd.FileName;          //How to show the path of the file
 
-            ofd.ShowDialog();
+                StreamReader Reader = new StreamReader(ofd.FileName);
 
-            txtRead.Text = ofd.FileName;
+                while(!Reader.EndOfStream)
+                {
+                    string x = Reader.ReadLine();
+                    strings.AddRange(x.Split(", "));    //The regular .Add() function doesn't work for substrings, so addrange must be used
+                    count++;
+                }
+
+                txtRead.Text = $"{count} Lines added to List";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);            //Shows exception message
+            }
+        }
+
+        private void AddRow()
+        {
+            double rowsNeeded = strings.Count / STRINGSPLITCOUNT;
+
+            for(int i = 0; i < rowsNeeded; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+
+        }
+
+        private void btnToGrid_Click(object sender, EventArgs e)
+        {
+            AddRow();
+
+            int count = 0;
+            int r = 0;          //Row integer 
+            int c = 0;          //Column integer
+
+                for(int i = 0; i < strings.Count; i++)
+                {
+                    dataGridView1.CurrentCell = dataGridView1.Rows[r].Cells[c];
+                    dataGridView1.CurrentCell.Value = strings[i];
+                    c++;
+
+                    if(c == STRINGSPLITCOUNT)
+                    {
+                        c = 0;
+                        r++;
+                    }
+                    count++;
+                }
+            txtCopy.Text = $"{count} Strings loaded to Datagrid";
+            dataGridView1.Refresh();
+
+            //string finalString = "";
+            //int stringPart = 0;
+
+
+            //dataGridView1.Rows.Clear();
+
+            //for(int i = 0; i < strings.Count; i++)
+            //{
+            //    strings[stringPart] += finalString;
+
+            //    if (stringPart == STRINGSPLITCOUNT)
+            //    {
+            //        stringPart = 0;
+            //    }
+
+            //    stringPart++;
+            //}
+
+
+            //for (int i = 0; !Reader.EndOfStream; i++)           //Whilst the reader is not end of stream, read each line
+            //{
+            //    string line = Reader.ReadLine();
+
+            //    dataGridView1.Rows.Add(line.Split(", "));       //Splits the line up and adds to each column
+
+            //    count++;
+            //}
+
+
+
         }
     }
 }
